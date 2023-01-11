dff449397bad3ea4882f55620bf56bb83aa82d7d
blubirch <anuragdashore>
2023-01-11 18:20:51 +0530

    Design home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-
-  get '/homes' => 'homes#index'
   root "homes#index"
+  get '/homes' => 'homes#index'
+  get '/blog' => 'homes#blog'
+  get '/about_us' => 'homes#about_us'
+  get '/event' => 'homes#event'
+  get '/contact_us' => 'homes#contact_us'
+  get '/speakear' => 'homes#speakear'
+  get '/ticket' => 'homes#ticket'
+  get '/schedule' => 'homes#schedule'
 end
